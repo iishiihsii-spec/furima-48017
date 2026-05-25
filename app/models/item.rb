c865f1b04f6a2ae00ed0b88c8cd5bb832e5fa43a
ko <iishiihsii@gmail.com>
2026-05-25 20:36:32 +0900

    Itemモデルにバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  # Activehash
+  # アソシエーション
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
@@ -10,4 +10,21 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  # バリデーション
+  validates :name, presence: true
+  validates :description, presence: true
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+  validates :price, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  }
+  validates :image, presence: true
 end
