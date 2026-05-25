dc34ab830ef1703b77609239b9cc72df55c0cad7
ko <iishiihsii@gmail.com>
2026-05-25 17:20:12 +0900

    アソシエーションを記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # Activehash
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   belongs_to :user
   has_one_attached :image
 end
