c68ab25df89d9cdf3dc3fdebbb42ad44a066b9c7
ko <iishiihsii@gmail.com>
2026-05-27 15:16:50 +0900

    「userと紐づいていなければ保存できない」ことを確認するテストを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -76,6 +76,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Image can't be blank"
       end
+      it 'userと紐づいていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'User must exist'
+      end
     end
   end
 end
