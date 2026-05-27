7aeb3f97200f2341a079a6d7438ef978b9b1fd9f
ko <iishiihsii@gmail.com>
2026-05-27 14:25:22 +0900

    Rubocopの指摘を修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -54,22 +54,22 @@ RSpec.describe Item, type: :model do
       it 'priceが300未満では出品できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+        expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
       end
       it 'priceが10,000,000以上では出品できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+        expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
       it 'priceが半角数字以外では出品できない' do
         @item.price = 'abc'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price is not a number"
+        expect(@item.errors.full_messages).to include 'Price is not a number'
       end
       it 'priceが全角数字では出品できない' do
         @item.price = '３００'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price is not a number"
+        expect(@item.errors.full_messages).to include 'Price is not a number'
       end
       it 'imageが空だと出品できない' do
         @item.image = nil
