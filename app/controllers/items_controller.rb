9d00e37288a0665c50501914f9598622c5dcc533
ko <iishiihsii@gmail.com>
2026-05-28 14:27:54 +0900

    editページのへのルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,6 +19,10 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
   end
 
   private
